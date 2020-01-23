4afb2010ddcb00b478c3f9368eac2a3e825e2966
hexanek <hexanek@gmail.com>
2020-01-23 18:05:37 +0100

    Change tittle

--- .history/src/components/Navbar_20200123175631.vue
+++ .history/src/components/Navbar_20200123175631.vue
@@ -29,7 +29,7 @@
       clipped-left
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>iParty</v-toolbar-title>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
     </div>
 </template>
