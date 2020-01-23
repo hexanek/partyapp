548b1b40c7754023fe4c857f78239af7d08ab233
hexanek <hexanek@gmail.com>
2020-01-23 18:53:49 +0100

    Add router

---
+++ .history/src/App_20200123185205.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <navbar></navbar>
+
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+
+
+  components: {
+    Navbar,
+    Footer
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
